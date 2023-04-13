46439887bf817ab987838d46b900ff17bd02fe38
StevenK418 <stevenkelly418@gmail.com>
2023-04-13 20:12:28 +0100

    => Updated test spec for employee api service.

--- src/app/employee/employee-list/employee-list.component.spec.ts
+++ src/app/employee/employee-list/employee-list.component.spec.ts
@@ -1,23 +1,62 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
-import { employeeListComponent } from './employee-list.component';
+import { Observable, of, throwError, retry, catchError } from 'rxjs';
+import { EmployeeAPIService } from 'src/app/services/employee-apiservice.service';
 
-describe('EmployeeListComponent', () => {
-  let component: employeeListComponent;
-  let fixture: ComponentFixture<employeeListComponent>;
+describe('EmployeeAPIService', () => {
+  let service: EmployeeAPIService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ employeeListComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(employeeListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    TestBed.configureTestingModule(
+      {
+        imports:[HttpClientTestingModule],
+        providers: [EmployeeAPIService]
+      });
+    service = TestBed.inject(EmployeeAPIService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+
+  it('Create an Employee on Service method', () => {
+    const service: EmployeeAPIService = TestBed.get(EmployeeAPIService);
+    let employeeServiceMock = spyOn(service, 'addNewEmployee').withArgs({    "_id": "642ea8d18fc35cee372cf0d5",
+    "firstName": "Jerrell",
+    "lastName": "Reichert",
+    "profilePicture": "https://loremflickr.com/640/480",
+    "email": "Ben_Rodriguez@yahoo.com",
+    "phoneNumber": "298.789.9748 x39346",
+    "address": "900 Trace Trace",
+    "status": true,
+    })
+      .and.returnValue(of({"_id": "642ea8d18fc35cee372cf0d5",
+      "firstName": "Jerrell",
+      "lastName": "Reichert",
+      "profilePicture": "https://loremflickr.com/640/480",
+      "email": "Ben_Rodriguez@yahoo.com",
+      "phoneNumber": "298.789.9748 x39346",
+      "address": "900 Trace Trace",
+      "status": true,
+      }))
+
+    service.addNewEmployee({"_id": "642ea8d18fc35cee372cf0d5",
+    "firstName": "Jerrell",
+    "lastName": "Reichert",
+    "profilePicture": "https://loremflickr.com/640/480",
+    "email": "Ben_Rodriguez@yahoo.com",
+    "phoneNumber": "298.789.9748 x39346",
+    "address": "900 Trace Trace",
+    "status": true}).subscribe((data) => {
+      console.log("called")
+      expect(data).toEqual(employeeServiceMock);
+    }); 
+    expect(service.addNewEmployee).toHaveBeenCalled();
+  
   });
 });
